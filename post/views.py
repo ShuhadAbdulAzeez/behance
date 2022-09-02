4a5fdc270a430737c65bc466cde310370db72aa5
Shuhad Abdul Azeez <shuhad04@gmail.com>
2022-09-02 16:59:24 +0530

    modified

--- post/views.py
+++ post/views.py
@@ -1,10 +1,12 @@
+from turtle import title
 from django.shortcuts import render
 from . models import Post
 
 def home(request):
     post = Post.objects.all()
+    print(post)
     context = {'post' :post}
-    return render(request, 'index.html', context)
+    return render(request, 'query.html', context)
 
 def post_description(request, pk):
     post = Post.objects.get(id=pk)
